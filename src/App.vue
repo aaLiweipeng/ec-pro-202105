8955d53c632ffbd0426cb657a45fe484bf91878e
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2021-05-09 21:28:33 +0800

    the first version

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="docker">
+    <span class="docker__item">首页</span>
+    <span class="docker__item">购物车</span>
+    <span class="docker__item">订单</span>
+    <span class="docker__item">我的</span>
+  </div>
+</template>
+
+<style lang="scss">
+.docker {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #F10000;
+  background: skyblue;
+}
+</style>
